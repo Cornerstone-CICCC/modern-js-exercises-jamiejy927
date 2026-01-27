a88f730ee9d3a9cb5228321e2044729f5fafdb3b
jamie <jamie927@gmail.com>
2026-01-27 11:39:59 -0800

    ex01 solved

--- exercises/ex01.js
+++ exercises/ex01.js
@@ -7,12 +7,30 @@ Create a function named sumLargestNumbers that will receive an array of numbers 
 */
 
 const sumLargestNumbers = function (data) {
+  // if (data.length < 2) {
+  //   return 'Less than 2 items';
+  // }
   // Put your solution here
-  return 11;
+
+  const sortedData = data.sort((a,b)=>{
+    if (a>b) {
+      return 1;
+    } else if (b>a) {
+      return -1;
+    } else return 0;
+  }).reverse();
+
+  // console.log("sortedData:", sortedData);
+
+  return result = sortedData[0] + sortedData[1];
+
+  // return result;
 };
 
-console.log(sumLargestNumbers([1, 10])); // 11
-console.log(sumLargestNumbers([1, 2, 3])); // 5
-console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2])); // 126
+// console.log(sumLargestNumbers([10]));
+// console.log(sumLargestNumbers([1, 10])); // 11
+// console.log(sumLargestNumbers([1, 2, 3])); // 5
+// console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2])); // 126
+// console.log(sumLargestNumbers([-10, -4, -34, -6, -92, -2])); // -6
 
 module.exports = sumLargestNumbers;
