7de89e24151230a6f5abd49921ffbfa7d35522d0
jamie <jamie927@gmail.com>
2026-02-11 11:46:47 -0800

    ex11 solved

--- exercises/ex11.js
+++ exercises/ex11.js
@@ -29,25 +29,48 @@ const PI = 3.14159;
 
 const sphereVolume = function (radius) {
   // Code here!
+  return (4 / 3) * PI * Math.pow(radius, 3);
 };
 
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189); //true
 
 const coneVolume = function (radius, height) {
   // And here!
+  return (1 / 3) * PI * Math.pow(radius, 2) * height;
 };
 
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49); //true
 
 const prismVolume = function (height, width, depth) {
   // Probably here too!
+  return height * width * depth;
 };
 
 console.log(prismVolume(3, 4, 5) === 60); //true
 
 const totalVolume = function (solids) {
   // Code here? Yup!
+  let totalVolume = 0;
+
+  for (const solid of solids) {
+    if (solid.type === "sphere") {
+      totalVolume += sphereVolume(solid.radius);
+    } else if (solid.type === "cone") {
+      totalVolume += coneVolume(solid.radius, solid.height);
+    } else if (solid.type === "prism") {
+      totalVolume += prismVolume(
+        solid.height,
+        solid.width,
+        solid.depth
+      );
+    }
+  }
+
+  return totalVolume;
 };
+
+// Space Complexity: O(1) = C = 1;
+// Time Complexity: O(n) ; n is solids size ; n is the input size
 
 const largeSphere = {
   type: "sphere",
